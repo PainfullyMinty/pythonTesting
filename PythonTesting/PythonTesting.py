63baa996eb384b76c64fcc8fbed35b826f7da54b
Angus Wilson <WILSONAR@sfx.act.edu.au>
2018-05-31 15:02:58 +1000

    changes

--- PythonTesting/PythonTesting.py
+++ PythonTesting/PythonTesting.py
@@ -1,2 +1,13 @@
-name = input("What is your name?\n")
+name = input("What is your name? \n")
 print("Hello " + name + "!")
+
+strAge = input("How old are you? \n")
+age = int(float(strAge))
+if age < 10:
+    print("Wow, " + str(age) + " is pretty young...")
+elif age >= 10 and age < 13:
+    print("You're pretty much a twelvie at " + str(age))
+elif age >= 13 and age < 20:
+    print("TEENAGER ALERT. THE AGE OF " + str(age) + " SUGGESTS HIGH LEVELS OF TEEN.")
+else:
+    print(str(age) + ". That's old.")
